676c07d45771afc7b70b9b8d6f6de0c2034d5684
vtloa <64365623+vtloa@users.noreply.github.com>
2025-03-08 12:32:55 +0300

    Update musicvideoprofile.js

--- twiberjs/musicvideoprofile.js
+++ twiberjs/musicvideoprofile.js
@@ -1,19 +1,3 @@
-<!-- Мелодия в профиле -->
-
-<script language="javascript">
-$('td#profile-right li:has(span:contains("Музыка"))').map(function(){var Ku=$('strong',this).html();
-if(Ku.match(/.mp3/)){plr=/(https?:\/\/[^\s<"]+?\.mp3)/gi; Ku=Ku.replace(plr,'<audio src="$1" controls style="width: 190px !important;"></audio>');};$('strong',this).html(Ku);});
-$('li[class^="pa-fld"]:contains("Музыка")').map(function(){
-var lm=$(this).text();if(!lm){return false;};
-if(lm.match(/.mp3/)){var plr=/Музыка\:(https?:\/\/[^\s<"]+?\.mp3)/gi;
-lm=lm.replace(plr,'Музыка:<br><audio src="$1" controls style="width: 190px !important;"></audio>');};$(this).html(lm);});
-</script>
-
-
-
-<!-- ВИДЕО в профиле -->
-
-<script language="javascript">
 $('td#profile-right li:has(span:contains("Видео"))').map(function() {
     var Ku = $('strong', this).html();
     if (Ku.match(/https?:\/\/(www\.)?youtube\.com\/watch\?v=/)) {
@@ -34,4 +18,3 @@ $('li[class^="pa-fld"]:contains("Видео")').map(function() {
     }
     $(this).html(lm);
 });
-</script>
